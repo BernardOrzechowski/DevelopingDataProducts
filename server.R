8edb114586fb26471dec3544443c9ebc20f2826b
BernardOrzechowski <bernard.orzechowski@gmail.com>
2014-06-21 08:09:16 +0200

    updated some logic

--- server.R
+++ server.R
@@ -1,27 +1,32 @@
 library(shiny)
 data(mtcars)
-variable_names <- names(mtcars)
+
 
 
 shinyServer(
       function(input, output, session) {
             output$outRegressors <- renderPrint(input$inRegressors)
             output$outRegressand <- renderPrint(input$regressand)
+            regressors <- do.call(paste, output$outRegressors, sep=" + "))
+
+            formulaText <- reactive({
+                  paste(input$regressand, regressors, sep=" ~ ")
+            })
+
+            fit <- lm(as.formula(formulaText()) , data = mtcars)
+            
             output$plot1 <- renderPlot({
-                  fit <- lm(mpg ~ . , data = mtcars); 
                   plot(fit, which = 1)
             })
-            output$plot2 <- renderPlot({
-                  fit <- lm(mpg ~ . , data = mtcars); 
+            
+            output$plot2 <- renderPlot({      
                   plot(fit, which = 2)
             })
             output$plot3 <- renderPlot({
-                  fit <- lm(mpg ~ . , data = mtcars); 
                   plot(fit, which = 3)
 
             })
             output$plot4 <- renderPlot({
-                  fit <- lm(mpg ~ . , data = mtcars); 
                   plot(fit, which = 4)
             })
             
--- ui.R
+++ ui.R
@@ -1,18 +1,36 @@
-shinyUI(pageWithSidebar(
-      headerPanel("Example plot"),
+shinyUI(fluidPage(
+      titlePanel("Regression model experiments on the mtcars data set"),
+      sidebarLayout(
       sidebarPanel(
             selectInput('inRegressors', 'Regressors', names(mtcars), multiple=TRUE, selectize=FALSE),
-            p("Chosen Predictor variables"),
-            verbatimTextOutput('outRegressors'),
-            selectInput('regressand', 'Regressand variable', names(mtcars), multiple=FALSE, selectize=FALSE),
-            verbatimTextOutput('outRegressand')
+            #p("Chosen Predictor variables"),
+            #verbatimTextOutput('outRegressors'),
+            selectInput('regressand', 'Regressand variable', names(mtcars), multiple=FALSE, selectize=FALSE)
+            #verbatimTextOutput('outRegressand')
       ),
       mainPanel(
-            plotOutput('plot1'),
-            plotOutput('plot2'),
-            plotOutput('plot3'),
-            plotOutput('plot4')
+            #plotOutput('plot1'),
+            #plotOutput('plot2'),
+            #plotOutput('plot3'),
+            #plotOutput('plot4')
+            fluidRow(
+                  column(8,
+                         plotOutput('plot1')      
+                  ),
+                  column(8,
+                         plotOutput('plot2')
+                  )
+            ),
+            fluidRow(
+                  column(8,
+                         plotOutput('plot3')      
+                  ),
+                  column(8,
+                         plotOutput('plot4')
+                  )
+            )
             
+      )
       )
 ))
 
